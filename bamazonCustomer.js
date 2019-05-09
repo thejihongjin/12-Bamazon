da59348fea11ab688470c6bcc26134571b217c28
thejihongjin <46304180+thejihongjin@users.noreply.github.com>
2019-05-08 20:51:46 -0700

    added insert statement to create product records in table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,3 +9,83 @@
 //    * This means updating the SQL database to reflect the remaining quantity.
 //    * Once the update goes through, show the customer the total cost of their purchase.
 // - - -
+
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "bamazon"
+});
+
+initialPrompt();
+
+function initialPrompt() {
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+
+        inquirer.prompt([
+            {
+                type: "rawlist",
+                message: "What product would you like to buy?",
+                choices: function () {
+                    var productArr = [];
+                    for (var i = 0; i < results.length; i++) {
+                        productArr.push(results[i].product_name); //push id/entire object?
+                    }
+                    return productArr;
+                },
+                name: "product"
+            },
+            {
+                type: "input",
+                message: "How many units would you like to buy?",
+                name: "quantity",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        ]).then(function (response) {
+            var chosenProduct;
+            for (var i = 0; i < results.length; i++) {
+                if (results[i].product_name === response.product) {
+                    chosenProduct = results[i];
+                }
+            }
+
+
+            // check if your store has enough of the product to meet the customer's request.
+            //    * If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
+            // 8. However, if your store _does_ have enough of the product, you should fulfill the customer's order.
+            //    * This means updating the SQL database to reflect the remaining quantity.
+            //    * Once the update goes through, show the customer the total cost of their purchase.
+            // if (parseInt(response.bid > chosenProduct.highest_bid)) {
+            //     connection.query(
+            //         "UPDATE auctions SET ? WHERE ?",
+            //         [
+            //             {
+            //                 highest_bid: chosenProduct.highest_bid
+            //             },
+            //             {
+            //                 id: chosenProduct.id
+            //             }
+            //         ],
+            //         function (err) {
+            //             if (err) throw err;
+            //             console.log("Bid placed successfully!");
+            //             initialPrompt();
+            //         }
+            //     );
+            // } else if (parseInt(response.bid <= chosenProduct.highest_bid)) {
+            //     console.log("Sorry, your bid was too low. Try again.");
+            //     initialPrompt();
+            // }
+        });
+    });
+}
--- bamazonSeeds.sql
+++ bamazonSeeds.sql
@@ -13,18 +13,17 @@ CREATE TABLE products (
     PRIMARY KEY (product_id)
 );
 
--- 4. Populate this database with around 10 different products. (i.e. Insert "mock" data rows into this database and table).
---INSERT INTO products (product_name, department_name, price, stock_quantity)
--- VALUES ("", "", 0, 0),
--- ("", "", 0, 0),
--- ("", "", 0, 0), 
--- ("", "", 0, 0),
--- ("", "", 0, 0), 
--- ("", "", 0, 0),
--- ("", "", 0, 0), 
--- ("", "", 0, 0),
--- ("", "", 0, 0), 
--- ("", "", 0, 0);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("hat", "clothing", 15, 10),
+-- ("", "books", 10, 10),
+("soccer ball", "sports", 20, 50), 
+("mp3 player", "electronics", 50, 20),
+("Spirited Away", "movies", 20, 5), 
+("bubble tea", "food", 5, 100),
+("coffee", "food", 5, 100), 
+("sweatshirt", "clothing", 25, 0),
+("laptop", "electronics", 500, 5), 
+("soccer cleats", "sports", 150, 5);
 
 CREATE TABLE departments (
     department_id INT NOT NULL AUTO_INCREMENT,
